8e6600ad46fed7fc5c63f748323c3d8840a88af5
mahammaali89@gmail.com <mahammaali89@gmail.com>
2024-05-20 09:39:34 +0300

    AddProjectToRepository

---
+++ Application/Common/Language.cs
@@ -0,0 +1,24 @@
+﻿using Models.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public class Language
+    {
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+
+        public List<Language> GetLanguages()
+        {
+            return new()
+            {
+                new Language { Code = CurrentLanguageCodeHandler.ArLanguageCode, Name = "Arabic" },
+                new Language { Code = CurrentLanguageCodeHandler.EnLanguageCode, Name = "English" }
+            };
+        }
+    }
+}
---
+++ Models/Custom/CurrentLanguageCodeHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Custom
+{
+    public static class CurrentLanguageCodeHandler
+    {
+        public static string ArLanguageCode = "en-GB";
+        public static string EnLanguageCode = "en";
+
+        public static string? CurrentLanguageCode { get; set; }
+    }
+}
